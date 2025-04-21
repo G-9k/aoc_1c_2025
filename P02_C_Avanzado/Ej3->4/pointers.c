b618577eb7282d646a33dc53e681dcf98de4dacf
Gonzalo <gaheredia@dc.uba.ar>
2025-04-21 13:19:26 -0300

    terminado ejercicio de punteros

---
+++ P02_C_Avanzado/Ej3->4/pointers.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main(){
+    int x = 42;
+    int *p = &x;
+
+
+    char *c = "Hola";
+
+    printf("Direccion de x: %p Valor: %d\n", (void*) &x, x);
+    printf("Direccion de p: %p Valor: %p\n", (void*) &p, (void*) p);
+    printf("Valor de lo que apunta p: %d\n", *p);
+    printf("Valor de lo que apunta c: %c\n", *c);
+}
